97819439ea9d8c375a9e594baf274f7a9495400d
wangfeyen <wangfeyen@gmail.com>
2023-08-01 13:59:49 -0300

    comentários

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import { gerarEmpresas } from "./gerar-empresas.mjs";
-const empresas = gerarEmpresas(4);
+
+import PromptSync from "prompt-sync"
+const prompt = PromptSync();
+
+console.log("Quantas empresas você quer gerar?")
+const qtddempresas = prompt("")
+const empresas = gerarEmpresas(qtddempresas);
+
 console.dir(empresas, { depth: null });
+
+//Soma-se todos os valores dos pedidos e se compara:
 let maiorsoma=0;
 let idmaiorvenda = 0;
 for (let i = 0; i < empresas.length; i++) {
    let soma = 0;
   const empresa = empresas[i];
-  console.log(" somatório da venda ",i);
+
+  //para a soma:
   for (let a = 0; a < empresa.pedidos.length; a++) {
     const val = +empresa.pedidos[a].valor;
     soma=soma+val
   }
+  console.log(" Somatório da venda ",i);
   console.log(soma)
+
+  //comparando e encontrando o maior:
   if(maiorsoma<soma){
    maiorsoma=soma
    idmaiorvenda = i
@@ -19,14 +32,11 @@ for (let i = 0; i < empresas.length; i++) {
 }
 
 console.log("A maior venda foi a venda de número ", idmaiorvenda)
-
-
-import PromptSync from "prompt-sync"
-const prompt = PromptSync(); 
+ 
 console.log("Você deseja saber mais detalhes sobre a venda? Responda 's' para sim e 'n' para não")
 const detal = prompt(" ")
-console.log("resposta:",detal)
-//if(detal==="s"){
-//  console.log("Dados da venda:")
-//  console.log(empresas[idmaiorvenda])
-//}
+//console.log("resposta:",detal)
+if(detal==="s"){
+  console.log("Dados da venda:")
+  console.log(empresas[idmaiorvenda])
+}
